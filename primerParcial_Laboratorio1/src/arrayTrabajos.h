e19cd7fd641279620181ad8e9f4302273d80b81e
Lucas Costanza <78046870+lucascostanza25@users.noreply.github.com>
2021-05-13 11:59:32 -0300

    ENTREGA FINAL

--- primerParcial_Laboratorio1/src/arrayTrabajos.h
+++ primerParcial_Laboratorio1/src/arrayTrabajos.h
@@ -42,15 +42,116 @@ typedef struct
 }eTrabajo;
 
 eTrabajo pedirDatosTrabajo();
+
+/**
+ * \brief
+ *
+ * \pre
+ * \post
+ * \param listaTrabajo
+ * \param tam del array
+ */
 void inicializarTrabajo(eTrabajo listaTrabajo[], int tam);
+
+/**
+ * \brief
+ *
+ * \pre
+ * \post
+ * \param
+ */
 void mostrarUnTrabajo(eTrabajo);
+
+/**
+ * \brief
+ *
+ * \pre
+ * \post
+ * \param listaTrabajo
+ * \param tam tam del array
+ * \return retorna estado
+ */
 int mostrarListadoTrabajos(eTrabajo listaTrabajo[], int tam);
+
+/**
+ * \brief
+ *
+ * \pre
+ * \post
+ * \param listaTrabajo
+ * \param tam del array
+ * \param idTrabajo autoincremental
+ * \return estado
+ */
 int altaTrabajo(eTrabajo listaTrabajo[], int tam, int *idTrabajo);
+
+/**
+ * \brief
+ *
+ * \pre
+ * \post
+ * \param listaTrabajo disponibles
+ * \param tam del array
+ * \return estado
+ */
 int buscarTrabajoLibre(eTrabajo listaTrabajo[], int tam);
+
+/**
+ * \brief
+ *
+ * \pre
+ * \post
+ * \param listaTrabajo
+ * \param tam del array
+ * \param listaServicio
+ * \param tam del array servicio
+ * \return
+ */
 int modificarTrabajo(eTrabajo listaTrabajo[], int tam, eServicio listaServicio[], int tamServicio);
+
+/**
+ * \brief
+ *
+ * \pre
+ * \post
+ * \param listaTrabajo
+ * \param tam del array
+ * \return estado
+ */
 int bajaTrabajo(eTrabajo listaTrabajo[], int tam);
+
+/**
+ * \brief
+ *
+ * \pre
+ * \post
+ * \param listaTrabajo
+ * \param tam del array
+ */
 void ordenarTrabajos(eTrabajo listaTrabajo[], int tam);
+
+/**
+ * \brief
+ *
+ * \pre
+ * \post
+ * \param listaTrabajo
+ * \param tamTrabajo
+ * \param listaServicios
+ * \param tamServicio
+ * \return estado
+ */
 int listadoTrabajosYServicios(eTrabajo listaTrabajo[], int tamTrabajo, eServicio listaServicios[], int tamServicio);
+
+/**
+ * \brief
+ *
+ * \pre
+ * \post
+ * \param listaServicios
+ * \param tam
+ * \return estado
+ */
 int listadoServicios(eServicio listaServicios[], int tam);
 
 
